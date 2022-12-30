a1f60fd6d7c5380d15e832605c6066f4d0401d5c
zied <hermi.zied@gmail.com>
2022-12-30 16:56:05 +0100

    first commit

--- build/app/generated/source/buildConfig/debug/com/ecoplus/gestion/BuildConfig.java
+++ build/app/generated/source/buildConfig/debug/com/ecoplus/gestion/BuildConfig.java
@@ -5,7 +5,7 @@ package com.ecoplus.gestion;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.ecoplus.gestion";
+  public static final String APPLICATION_ID = "com.demo.accountingapp";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 13;
   public static final String VERSION_NAME = "2.0.0";
